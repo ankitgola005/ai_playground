d7f737e156d102ce12f5e40cde64c0610f25fda6
Ankit Gola <ankitgola005@gmail.com>
2026-03-19 21:23:53 +0530

    update typing

---
+++ src/ai_playground/configs/__init__.py
@@ -0,0 +1,16 @@
+from ai_playground.configs.config import (
+    ConfigProtocol,
+    DataConfigProtocol,
+    DistributedConfigProtocol,
+    ModelConfigProtocol,
+    TrainerConfigProtocol,
+    ExperimentalConfigProtocol,
+)
+
+__all__ = [
+    "ConfigProtocol",
+    "DataConfigProtocol",
+    "DistributedConfigProtocol",
+    "ModelConfigProtocol",
+    "ExperimentalConfigProtocol",
+]
